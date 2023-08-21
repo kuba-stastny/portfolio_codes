b2152a47a77ba40d39595e7ea4244bec844be081
kuba-stastny <66886387+kuba-stastny@users.noreply.github.com>
2023-08-21 20:15:32 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jakub Šťastný</title>
+    <link rel="stylesheet" href="styl.css">
+    <link rel="icon" type="image/x-icon" href="pictures/purple.png">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@900&display=swap" rel="stylesheet">
+
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet">
+</head>
+
+
+<body>
+    <?php include("parts/gradientScreen.php") ?>
+
+    <?php include("parts/menu.php") ?>
+
+    <?php include("parts/slide.php") ?>
+
+    <?php include("parts/services2.php") ?>
+
+    <?php include("parts/reference.php") ?>
+
+    <?php include("parts/prices.php") ?>
+
+</body>
+
+</html>
